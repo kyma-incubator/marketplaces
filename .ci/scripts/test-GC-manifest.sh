e4057c38388702065d565d24b986d592e55b959b
pprecel <filip.strozik@outlook.com>
2019-10-24 10:49:17 +0200

    Change log line position

--- .ci/scripts/test-GC-manifest.sh
+++ .ci/scripts/test-GC-manifest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eox
 
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 log "${BASH_SOURCE[0]}"
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$( cd "${SCRIPTS_DIR}/../cluster" && pwd )"
 ARTIFACTS="${ARTIFACTS:-"${ROOT_DIR}/in"}"
 INSTALLATIONTIMEOUT=1800 #in this case it mean 30 minutes
