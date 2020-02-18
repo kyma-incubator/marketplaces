a704d11d31d8e65e8431a860efcba53a0815ab59
Thaler, Andreas <andreas.thaler01@sap.com>
2020-02-18 12:30:37 +0100

    update

--- gardener-addon/files/installation.sh
+++ gardener-addon/files/installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "$(kubectl -n kyma-installer get job -l app=kyma-initializer,version!=${KYMA_VERSION})" ]; then
-    echo "---> Applying Kyma-Installer for ${KYMA_VERSION}"
+if [ -z "$(kubectl -n kyma-installer get job -l app=kyma-initializer,version!=${KYMA_VERSION} --ignore-not-found)" ]; then
+    echo "---> Apply Kyma-Installer for ${KYMA_VERSION}"
     kubectl apply -f "https://raw.githubusercontent.com/kyma-project/kyma/${KYMA_VERSION}/installation/resources/installer.yaml"
 
     echo "---> Apply Kyma installation CR for ${KYMA_VERSION}"
     kubectl apply -f "https://raw.githubusercontent.com/kyma-project/kyma/${KYMA_VERSION}/installation/resources/installer-cr-cluster.yaml.tpl"
 else
-    echo "---> Skipping Kyma installation as there is a Kyma installation already"
+    echo "---> Skip Kyma installation as there is already a Kyma installation"
 fi
--- gardener-addon/files/tiller.sh
+++ gardener-addon/files/tiller.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-if [ -z "$(kubectl -n kyma-installer get job -l app=kyma-initializer,version!=${KYMA_VERSION})" ]; then
-    echo "---> Applying Tiller for ${KYMA_VERSION}"
+if [ -z "$(kubectl -n kyma-installer get job -l app=kyma-initializer,version!=${KYMA_VERSION} --ignore-not-found)" ]; then
+    echo "---> Apply Tiller for ${KYMA_VERSION}"
     kubectl apply -f "https://raw.githubusercontent.com/kyma-project/kyma/${KYMA_VERSION}/installation/resources/tiller.yaml"
 else
-    echo "---> Skipping Tiller installation as there is a Kyma installation already"
+    echo "---> Skip Tiller installation as there is already a Kyma installation"
 fi
